5b3dfff196803c9e4c284f6f7e093a580c8dd9e3
Markus Kohlhase <mail@markus-kohlhase.de>
2011-05-21 12:54:36 +0200

    Added compact view with isotope plogin. Fixes #18

--- src/modules/list/list.js
+++ src/modules/list/list.js
@@ -213,7 +213,7 @@ fast.modules.list = fast.modules.list || (function( window, undefined ){
       c.delegate( "button.done",'click', undo );
       c.delegate( "button.favorite.inactive",'click', favEnable );
       c.delegate( "button.favorite.active",'click', favDisable );
-      c.delegate( "li",'click', select );
+      c.delegate( "div.item",'click', select );
       ulTmpl = sb.getTemplate("list");
       model.subscribe( this );
     };
@@ -271,19 +271,29 @@ fast.modules.list = fast.modules.list || (function( window, undefined ){
     /**
     * Function: update
     */
-    var update = function(){
-      c.empty();
+		var update = function(){
+			c.empty();
 
-      for( var i in model.grouped ){
+			for( var i in model.grouped ){
 				var entries = {};
 					for( var j in model.grouped[i] ){
 						entries[j] = model.collection[j];
 				}
 				if( sb.count(entries) > 0){
-					sb.tmpl( ulTmpl, { title: i ,entries: entries, selected: model.selected[0]  } ).appendTo( c );
+					sb.tmpl( ulTmpl, { title: i ,entries: entries, selected: model.selected[0] } ).appendTo( c );
 				}
-      };
-    };
+			}
+			c.find(".items").isotope({
+				animationEngine : 'best-available',
+				itemSelector: '.item',
+				layoutMode : 'cellsByRow',
+				resizable: true,
+				cellsByRow : {
+					columnWidth : 240,
+					rowHeight : 25
+				},
+			});
+		};
 
     // public API
     return({
